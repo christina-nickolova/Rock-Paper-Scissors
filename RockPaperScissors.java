3159b03080cafe6bf2b78b38f4b8d17b6692eb40
christina-nickolova <christinaxnickolova@gmail.com>
2023-01-23 22:33:52 +0200

    Update RockPaperScissors.java
    Added "End Game" option, score and played games calculation, game title and some visual changes

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -10,42 +10,73 @@ public class RockPaperScissors {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("--------------------------");
+        System.out.println("~ROCK PAPER SCISSORS GAME~");
+        System.out.println("--------------------------");
+        System.out.println();
+
         System.out.print("Choose [r]ock, [p]aper or [s]cissors: ");
         String playerMove = scanner.nextLine();
 
-        switch (playerMove) {
-            case "r", "rock" -> playerMove = ROCK;
-            case "p", "paper" -> playerMove = PAPER;
-            case "s", "scissors" -> playerMove = SCISSORS;
-            default -> {
-                System.out.println("Invalid Input. Try Again...");
-                return;
+        int playerScore = 0;
+        int computerScore = 0;
+        int playedGames = 0;
+
+        while (!playerMove.matches("^(?i:end game)$")) {
+
+            switch (playerMove) {
+                case "r", "rock" -> {
+                    playerMove = ROCK;
+                    playedGames++;
+                }
+                case "p", "paper" -> {
+                    playerMove = PAPER;
+                    playedGames++;
+                }
+                case "s", "scissors" -> {
+                    playerMove = SCISSORS;
+                    playedGames++;
+                }
+                default -> {
+                    System.out.println("Invalid Input. Try Again...");
+                    playerMove = scanner.nextLine();
+                    continue;
+                }
             }
-        }
-        Random random = new Random();
-        int computerRandomNumber = random.nextInt(4);
+            Random random = new Random();
+            int computerRandomNumber = random.nextInt(4);
 
-        String computerMove;
+            String computerMove;
 
-        if (computerRandomNumber == 1) {
-            computerMove = ROCK;
-        } else if (computerRandomNumber == 2) {
-            computerMove = PAPER;
-        } else {
-            computerMove = SCISSORS;
-        }
-        System.out.printf("The computer chose %s.%n", computerMove);
+            if (computerRandomNumber == 1) {
+                computerMove = ROCK;
+            } else if (computerRandomNumber == 2) {
+                computerMove = PAPER;
+            } else {
+                computerMove = SCISSORS;
+            }
+            System.out.printf("The computer chose %s.%n", computerMove);
 
-        if ((playerMove.equals("Rock") && computerMove.equals("Scissors")) ||
-                (playerMove.equals("Paper") && computerMove.equals("Rock")) ||
-                (playerMove.equals("Scissors") && computerMove.equals("Paper"))) {
-            System.out.println("You win.");
-        } else if ((playerMove.equals("Rock") && computerMove.equals("Rock")) ||
-                (playerMove.equals("Paper") && computerMove.equals("Paper")) ||
-                (playerMove.equals("Scissors") && computerMove.equals("Scissors"))) {
-            System.out.println("This game was a draw.");
-        } else {
-            System.out.println("You lose.");
+            if ((playerMove.equals("Rock") && computerMove.equals("Scissors")) ||
+                    (playerMove.equals("Paper") && computerMove.equals("Rock")) ||
+                    (playerMove.equals("Scissors") && computerMove.equals("Paper"))) {
+                playerScore++;
+                System.out.println("You win :)");
+            } else if ((playerMove.equals("Rock") && computerMove.equals("Rock")) ||
+                    (playerMove.equals("Paper") && computerMove.equals("Paper")) ||
+                    (playerMove.equals("Scissors") && computerMove.equals("Scissors"))) {
+                playerScore++;
+                computerScore++;
+                System.out.println("This game was a draw.");
+            } else {
+                computerScore++;
+                System.out.println("You lose :(");
+            }
+            playerMove = scanner.nextLine();
         }
+        System.out.println("---RESULTS---");
+        System.out.printf("Your score: %d%nComputer score: %d%n", playerScore, computerScore);
+        System.out.println("-------------");
+        System.out.println("Played games: " + playedGames);
     }
 }
